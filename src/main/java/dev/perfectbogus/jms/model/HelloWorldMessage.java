53a5db0627e1815efa78b2f06b11809db309ee71
eduardo escobar <war_army_10@hotmail.com>
2022-04-20 00:03:38 -0500

    Adding little example

---
+++ src/main/java/dev/perfectbogus/jms/model/HelloWorldMessage.java
@@ -0,0 +1,22 @@
+package dev.perfectbogus.jms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = 3444074367340559971L;
+
+    private UUID id;
+    private String message;
+
+}
